6c9d56773a460c75e4caf35394f113520b1a4199
HMZ <hamzi1995@gmail.com>
2022-04-30 23:31:26 +0300

    Add window drag behavior + a hover effect for the close button

--- NetStalkerAvalonia/Components/TitleBar.axaml.cs
+++ NetStalkerAvalonia/Components/TitleBar.axaml.cs
@@ -1,13 +1,19 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 
 namespace NetStalkerAvalonia.Components
 {
     public partial class TitleBar : UserControl
     {
+        private Window? _window;
+
         public TitleBar()
         {
             InitializeComponent();
+
+            this.Initialized += (sender, args) => { AttachDraggingBehaviour(); };
         }
 
         private void InitializeComponent()
@@ -35,6 +41,29 @@ namespace NetStalkerAvalonia.Components
         private void CloseWindow(Window window)
         {
             window.Close();
+        }
+
+        private void AttachDraggingBehaviour()
+        {
+            this.PointerPressed += TitleBar_PointerPressed;
+        }
+
+        private void TitleBar_PointerPressed(object? sender, Avalonia.Input.PointerPressedEventArgs e)
+        {
+            var mouseDevice = e.GetCurrentPoint(this);
+
+            if (mouseDevice.Properties.IsLeftButtonPressed)
+            {
+                if (_window == null)
+                {
+                    _window = (Application.Current?.ApplicationLifetime
+                                 as IClassicDesktopStyleApplicationLifetime)?.MainWindow;
+
+                    return;
+                }
+
+                _window.BeginMoveDrag(e);
+            }
         }
     }
 }
