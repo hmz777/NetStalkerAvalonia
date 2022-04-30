6c9c98c26a7fffac4bce0993e9dddfd737e884a3
HMZ <hamzi1995@gmail.com>
2022-04-30 23:36:28 +0300

    Add a test device class in order to populate with dummy data

---
+++ NetStalkerAvalonia/Models/Device.cs
@@ -0,0 +1,13 @@
+﻿namespace NetStalkerAvalonia.Models
+{
+    public class Device
+    {
+        public string IP { get; set; }
+        public string Mac { get; set; }
+        public bool Blocked { get; set; }
+        public bool Redirected { get; set; }
+        public string Download { get; set; }
+        public string Upload { get; set; }
+        public string Name { get; set; }
+    }
+}
--- NetStalkerAvalonia/ViewModels/MainWindowViewModel.cs
+++ NetStalkerAvalonia/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,8 @@
+using NetStalkerAvalonia.Components.DeviceList;
+using NetStalkerAvalonia.Models;
 using NetStalkerAvalonia.ViewModels.RoutedViewModels;
 using ReactiveUI;
+using System.Collections.Generic;
 using System.Reactive;
 
 namespace NetStalkerAvalonia.ViewModels
@@ -30,10 +33,41 @@ namespace NetStalkerAvalonia.ViewModels
             // your view models, or to reuse existing view models.
             //
 
+            // Navigation wiring
+
             GoToSniffer = ReactiveCommand.CreateFromObservable(
                 () => Router.Navigate.Execute(new SnifferViewModel(this))
             );
+
+            // Dummy data population
+            PopulateDummyData(Devices);
         }
 
+        public ICollection<Device> Devices { get; set; } = new List<Device>();
+        public DeviceListViewSettings DeviceListViewSettings { get; set; } = new();
+
+        #region Testing
+
+        private static ICollection<Device> PopulateDummyData(ICollection<Device> devices)
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                devices.Add(
+                     new Device()
+                     {
+                         IP = $"192.168.1.{i}",
+                         Mac = $"fffffff{i}",
+                         Blocked = i % 2 == 0,
+                         Redirected = i + 1 % 2 == 0,
+                         Download = $"{i} Kb/s",
+                         Upload = $"{i} Kb/s",
+                         Name = $"Dev {i}"
+                     });
+            }
+
+            return devices;
+        }
+
+        #endregion
     }
 }
