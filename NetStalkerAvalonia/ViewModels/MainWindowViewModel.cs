80f8b7be7a0b24f2ee9993c12f1fb6c345088895
HMZ <hamzi1995@gmail.com>
2022-05-03 20:09:12 +0300

    Wire the router bindings so we detect page changes and the ability to go back + code refactorings

--- NetStalkerAvalonia/ViewModels/MainWindowViewModel.cs
+++ NetStalkerAvalonia/ViewModels/MainWindowViewModel.cs
@@ -4,44 +4,46 @@ using NetStalkerAvalonia.ViewModels.RoutedViewModels;
 using ReactiveUI;
 using System.Collections.Generic;
 using System.Reactive;
+using System.Reactive.Linq;
 
 namespace NetStalkerAvalonia.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase, IScreen
     {
+        #region Routing
+
         // The Router associated with this Screen.
         // Required by the IScreen interface.
         public RoutingState Router { get; } = new RoutingState();
 
         // Commands to navigate the different views
-        public ReactiveCommand<Unit, IRoutableViewModel> GoToHome { get; }
-        public ReactiveCommand<Unit, IRoutableViewModel> GoToRules { get; }
-        public ReactiveCommand<Unit, IRoutableViewModel> GoToSniffer { get; }
-        public ReactiveCommand<Unit, IRoutableViewModel> GoToOptions { get; }
-        public ReactiveCommand<Unit, IRoutableViewModel> GoToHelp { get; }
+        public ReactiveCommand<Unit, IRoutableViewModel>? GoToHome { get; }
+        public ReactiveCommand<Unit, IRoutableViewModel>? GoToRules { get; }
+        public ReactiveCommand<Unit, IRoutableViewModel>? GoToSniffer { get; }
+        public ReactiveCommand<Unit, IRoutableViewModel>? GoToOptions { get; }
+        public ReactiveCommand<Unit, IRoutableViewModel>? GoToHelp { get; }
 
         // The command that navigates a user back.
         public ReactiveCommand<Unit, Unit> GoBack => Router.NavigateBack;
+        public void GoBackHandler() { GoBack?.Execute(); }
 
-        public MainWindowViewModel()
-        {
-            // Manage the routing state. Use the Router.Navigate.Execute
-            // command to navigate to different view models. 
-            //
-            // Note, that the Navigate.Execute method accepts an instance 
-            // of a view model, this allows you to pass parameters to 
-            // your view models, or to reuse existing view models.
-            //
+        private readonly ObservableAsPropertyHelper<bool> _canGoBack;
+        public bool CanGoBack => _canGoBack.Value;
 
-            // Navigation wiring
+        private Stack<string> ViewNames = new();
 
-            GoToSniffer = ReactiveCommand.CreateFromObservable(
-                () => Router.Navigate.Execute(new SnifferViewModel(this))
-            );
+        #endregion
 
-            // Dummy data population
-            PopulateDummyData(Devices);
-        }
+        #region Properties
+
+        #region UI Info
+
+        private readonly ObservableAsPropertyHelper<string> _pageTitle;
+        public string PageTitle => _pageTitle.Value;
+
+        #endregion
+
+        #region Devices List
 
         public ICollection<Device> Devices { get; set; } = new List<Device>();
         public DeviceListViewSettings DeviceListViewSettings { get; set; } = new();
@@ -66,6 +68,43 @@ namespace NetStalkerAvalonia.ViewModels
             }
 
             return devices;
+        }
+
+        #endregion
+
+        #endregion
+
+        #endregion
+
+        #region Constructor
+
+        public MainWindowViewModel()
+        {
+            // Info wiring
+            _pageTitle = this.WhenAnyObservable(x => x.Router.CurrentViewModel)
+                             .Select(x => GetPageNameFromViewModel(x))
+                             .ToProperty(this, x => x.PageTitle);
+
+            // Navigation wiring
+            _canGoBack = this.WhenAnyValue(x => x.Router.NavigationStack.Count)
+                             .Select(count => count > 0)
+                             .ToProperty(this, x => x.CanGoBack);
+
+            GoToSniffer = ReactiveCommand.CreateFromObservable(
+                () => { ViewNames.Push("Packet Sniffer"); return Router.Navigate.Execute(new SnifferViewModel(this)); });
+
+            // Dummy data population
+            PopulateDummyData(Devices);
+        }
+
+        #endregion
+
+        #region Tools
+
+        //We get the view name, othewise we return the initial name
+        private static string GetPageNameFromViewModel(IRoutableViewModel routableViewModel)
+        {
+            return routableViewModel?.UrlPathSegment ?? "Device List";
         }
 
         #endregion
