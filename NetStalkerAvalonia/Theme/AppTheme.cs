ac007f523a052cf10f2f73bb5c888ac22c27cd3d
HMZ <hamzi1995@gmail.com>
2022-05-04 20:15:59 +0300

    Update the app theme

--- NetStalkerAvalonia/Theme/AppTheme.cs
+++ NetStalkerAvalonia/Theme/AppTheme.cs
@@ -4,10 +4,11 @@
     {
         public static string WindowWidth => "800";
         public static string WindowHeight => "600";
-        public static string NavBackground => "#1F1F1F";
-        public static string NavElementBackground => "#333333";
-        public static string NavElementBorder => "#424242";
-        public static string NavElementText => "white";
+        public static string NavBorderColor => "#1F1F1F";
+        public static string NavBackground => "#000000";//1F1F1F
+        public static string NavElementBackground => "#000000";//333333
+        public static string NavElementBorder => "#1F1F1F";
+        public static string NavElementText => "#ffffff";
         public static string WindowBackground => "#000000";
         public static string TitleBarBackColor => "#000000";
         public static string TitleBarButtonBackColor => "#000000";
