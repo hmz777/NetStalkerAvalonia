77ba7daf480f9b2bf8f3c6e72df0f01babfa8ba8
HMZ <hamzi1995@gmail.com>
2022-05-05 17:51:48 +0300

    Update the device list view to use a device type to bitmap icon converter in order to identify devices with images in the list

---
+++ NetStalkerAvalonia/Converters/ListTypeToIconConverter.cs
@@ -0,0 +1,33 @@
+﻿using Avalonia;
+using Avalonia.Data.Converters;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using NetStalkerAvalonia.Models;
+using System;
+using System.Globalization;
+using System.Reflection;
+
+namespace NetStalkerAvalonia.Converters
+{
+    public class ListTypeToIconConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+
+            var type = (DeviceType)value;
+
+            var assets = AvaloniaLocator.Current.GetService<IAssetLoader>();
+
+            var fileName = $"/Assets/DeviceListIcons/{type.ToString().ToLower()}.png";
+
+            return new Bitmap(assets.Open(
+                     new Uri(string.Format("avares://{0}{1}", Assembly.GetExecutingAssembly().GetName().Name, fileName))));
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
