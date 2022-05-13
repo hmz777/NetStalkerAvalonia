8c8ea291dd44eac0f7928c64b99e602fa28b2d26
HMZ <hamzi1995@gmail.com>
2022-05-13 18:31:45 +0300

    Configure Splat container and register required app services and options + register logging

--- NetStalkerAvalonia/Program.cs
+++ NetStalkerAvalonia/Program.cs
@@ -1,9 +1,19 @@
 using Avalonia;
 using Avalonia.ReactiveUI;
+using NetStalkerAvalonia.Services;
+using NetStalkerAvalonia.Services.Implementations.BandwidthControl;
+using NetStalkerAvalonia.Services.Implementations.BlockingRedirection;
+using NetStalkerAvalonia.Services.Implementations.DeviceNameResolving;
+using NetStalkerAvalonia.Services.Implementations.DeviceTypeIdentification;
+using NetStalkerAvalonia.Services.Implementations.Notifications;
+using NetStalkerAvalonia.Services.Implementations.Packets;
 using ReactiveUI;
+using Serilog;
 using Splat;
 using System;
+using System.Configuration;
 using System.Reflection;
+using ILogger = Serilog.ILogger;
 
 namespace NetStalkerAvalonia
 {
@@ -24,10 +34,40 @@ namespace NetStalkerAvalonia
             //
             Locator.CurrentMutable.RegisterViewsForViewModels(Assembly.GetExecutingAssembly());
 
+            // Configure logging
+            ConfigureLogging();
+
+            // Register app services
+            RegisterRequiredServices();
+
             return AppBuilder.Configure<App>()
                  .UsePlatformDetect()
                  .LogToTrace()
                  .UseReactiveUI();
+        }
+
+        private static void ConfigureLogging()
+        {
+            var log = new LoggerConfiguration()
+                                .WriteTo.Console()
+                                .WriteTo.File(Assembly.GetExecutingAssembly().Location, rollingInterval: RollingInterval.Day)
+                                .CreateLogger();
+
+            Locator.CurrentMutable.Register<ILogger>(() => log);
+        }
+
+        private static void RegisterRequiredServices()
+        {
+            Locator.CurrentMutable.Register<IBandwidthController>(() => new BandwidthController());
+            Locator.CurrentMutable.Register<IBlockerRedirector>(() => new BlockerRedirector());
+            Locator.CurrentMutable.Register<IDeviceNameResolver>(() => new DeviceNameResolver());
+            Locator.CurrentMutable.Register<IDeviceTypeIdentifier>(() => new DeviceTypeIdentifier());
+
+            var notificationOptions = ConfigurationManager.GetSection("Notifications") as NotificationOptions
+                                      ?? new NotificationOptions();
+
+            Locator.CurrentMutable.Register<INotificationManager>(() => new NotificationManager(notificationOptions));
+            Locator.CurrentMutable.Register<IPacketManager>(() => new PacketManager());
         }
     }
 }
