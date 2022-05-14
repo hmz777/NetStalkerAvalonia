ae23e2fbc0a61697542723860685462e6c49fae6
HMZ <hamzi1995@gmail.com>
2022-05-14 19:08:34 +0300

    Add Converters to convert IpAddress and PhysicalAddress types to/from string when binding to list

---
+++ NetStalkerAvalonia/Converters/PhysicalAddressToStringConverter.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Net.NetworkInformation;
+
+namespace NetStalkerAvalonia.Converters
+{
+    public class PhysicalAddressToStringConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is not PhysicalAddress phAddress)
+                return null;
+
+            return string.Concat(phAddress.GetAddressBytes().Select(by => string.Format("{0}-", by.ToString("X2"))).ToArray()).TrimEnd('-');
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (string.IsNullOrEmpty(value?.ToString()))
+                return null;
+
+            return PhysicalAddress.Parse(value.ToString());
+        }
+    }
+}
