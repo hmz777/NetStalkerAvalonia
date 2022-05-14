e13c5061b8f2e49f385fc64b6696de5cada2e16e
HMZ <hamzi1995@gmail.com>
2022-05-14 19:09:10 +0300

    Update AppTheme.cs

--- NetStalkerAvalonia/Theme/AppTheme.cs
+++ NetStalkerAvalonia/Theme/AppTheme.cs
@@ -2,20 +2,22 @@
 {
     public class AppTheme
     {
-        public static string WindowWidth => "800";
+        public static string WindowWidth => "900";
         public static string WindowHeight => "600";
-        public static string NavBorderColor => "#1F1F1F";
-        public static string NavBackground => "#000000";
-        public static string NavElementBackground => "#000000";
+        public static string NavBorderColor => "#2C2C2C";
+        public static string NavBackground => "#1E1E1E";
+        public static string NavElementBackground => "#2C2C2C";
         public static string NavElementBorder => "#1F1F1F";
         public static string NavElementText => "#ffffff";
-        public static string WindowBackground => "#000000";
+        public static string WindowBackground => "#1E1E1E";
         public static string TitleBarBackColor => "#000000";
         public static string TitleBarButtonBackColor => "#000000";
         public static string TitleBarButtonFrontColor => "#FFFFFF";
         public static string TitleBarButtonsHoverColor => "#1F1F1F";
         public static string PageTitleColor => "white";
+        public static string AccentColor => "Teal";
         public static string PageTitleSize => "35";
         public static string ListRowSelectedBackColor => "Teal";
+        public static string FontFamily => "Verdana";
     }
 }
