9f6f0de0dadef031653ea9d1d0e790f49e9d1f55
HMZ <hamzi1995@gmail.com>
2022-05-21 20:26:29 +0300

    Add a configuration type to be binded from app config file

---
+++ NetStalkerAvalonia/Configuration/ApplicationConfiguration.cs
@@ -0,0 +1,7 @@
+namespace NetStalkerAvalonia.Configuration;
+
+public class ApplicationConfiguration
+{
+    public static bool SpoofProtection { get; set; }
+    public static bool SuppressNotifications { get; set; }
+}
