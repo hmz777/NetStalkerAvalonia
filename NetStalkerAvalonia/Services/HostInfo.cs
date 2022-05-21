3645e21ade64bac01c43f83b12910c7808a2a15c
HMZ <hamzi1995@gmail.com>
2022-05-21 20:28:30 +0300

    Add more helper members to the HostInfo type

--- NetStalkerAvalonia/Services/HostInfo.cs
+++ NetStalkerAvalonia/Services/HostInfo.cs
@@ -1,6 +1,7 @@
 ﻿using NetStalkerAvalonia.Models;
 using System.Net;
 using System.Net.NetworkInformation;
+using NetStalkerAvalonia.Helpers;
 
 namespace NetStalkerAvalonia.Services
 {
@@ -10,7 +11,9 @@ namespace NetStalkerAvalonia.Services
         public static PhysicalAddress? GatewayMac { get; set; }
 
         public static IPAddress? HostIp { get; set; }
+        public static string? RootIp => Tools.GetRootIp(HostIp!);
         public static PhysicalAddress? HostMac { get; set; }
+        public static PhysicalAddress? EmptyMac { get; set; } = PhysicalAddress.Parse("00-00-00-00-00-00");
 
         public static NetworkClass NetworkClass { get; set; }
         public static IPAddress? SubnetMask { get; set; }
