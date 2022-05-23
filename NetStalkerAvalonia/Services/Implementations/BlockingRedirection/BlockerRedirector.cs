6d9c9715b30bc660835fce66152f9105e9bd7b4b
HMZ <hamzi1995@gmail.com>
2022-05-23 20:31:59 +0300

    Implement the rest of the BlockerRedirector functionality

--- NetStalkerAvalonia/Services/Implementations/BlockingRedirection/BlockerRedirector.cs
+++ NetStalkerAvalonia/Services/Implementations/BlockingRedirection/BlockerRedirector.cs
@@ -1,10 +1,12 @@
 ﻿using NetStalkerAvalonia.Models;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Reactive;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using DynamicData;
+using NetStalkerAvalonia.Configuration;
+using PacketDotNet;
 using ReactiveUI;
 using SharpPcap;
 using SharpPcap.LibPcap;
@@ -13,17 +15,27 @@ namespace NetStalkerAvalonia.Services.Implementations.BlockingRedirection
 {
     public class BlockerRedirector : IBlockerRedirector
     {
+        #region Members
+
         private CancellationTokenSource? _cancellationTokenSource;
         private bool _isStarted;
         private LibPcapLiveDevice? _device;
 
-        private List<Device>? Clients { get; set; }
+        private ReadOnlyObservableCollection<Device>? _clients;
+
+        #endregion
+
+        #region Constructor
 
         public BlockerRedirector()
         {
             InitDevice();
             BindClients();
         }
+
+        #endregion
+
+        #region Internal
 
         private void InitDevice()
         {
@@ -36,16 +48,60 @@ namespace NetStalkerAvalonia.Services.Implementations.BlockingRedirection
             }
         }
 
-        private void DeviceOnOnPacketArrival(object sender, PacketCapture e)
-        {
-            // TODO: Implement packet processing logic
-        }
-
         private void BindClients()
         {
-            this.WhenAnyObservable(x => MessageBus.Current
-                    .Listen<ReadOnlyObservableCollection<Device>>(null))
-                .BindTo(this, x => x.Clients);
+            this.WhenAnyObservable(x => MessageBus
+                    .Current.Listen<IChangeSet<Device, string>>(null))
+                .BindTo(this, x => x._clients);
+        }
+
+        private void DeviceOnOnPacketArrival(object sender, PacketCapture e)
+        {
+            if (_isStarted == false)
+                return;
+
+            var rawCapture = e.GetPacket();
+            var packet = Packet.ParsePacket(rawCapture.LinkLayerType, rawCapture.Data) as EthernetPacket;
+
+            if (packet == null)
+                return;
+
+            var outTarget = _clients?
+                .FirstOrDefault(x => x.Mac!.Equals(packet.SourceHardwareAddress));
+
+            if (outTarget is not null
+                && outTarget is { Redirected: true }
+                && outTarget.IsLocalDevice() == false
+                && outTarget.IsGateway() == false)
+            {
+                if (outTarget.Upload == 0 || outTarget.Upload > outTarget.BytesSentSinceLastReset)
+                {
+                    packet.SourceHardwareAddress = HostInfo.HostMac;
+                    packet.DestinationHardwareAddress = HostInfo.GatewayMac;
+                    _device.SendPacket(packet);
+                    outTarget.SetSentBytes(packet.Bytes.Length);
+                }
+            }
+            else if (packet.SourceHardwareAddress.Equals(HostInfo.GatewayMac))
+            {
+                var ipv4Packet = packet.Extract<IPv4Packet>();
+                var inTarget = _clients?
+                    .FirstOrDefault(x => x.IP!.Equals(ipv4Packet.DestinationAddress));
+
+                if (inTarget is not null
+                    && inTarget is { Redirected: true }
+                    && inTarget.IsLocalDevice() == false
+                    && inTarget.IsGateway() == false)
+                {
+                    if (inTarget.Download == 0 || inTarget.Download > inTarget.BytesReceivedSinceLastReset)
+                    {
+                        packet.SourceHardwareAddress = HostInfo.HostMac;
+                        packet.DestinationHardwareAddress = inTarget.Mac;
+                        _device.SendPacket(packet);
+                        inTarget.SetReceivedBytes(packet.Bytes.Length);
+                    }
+                }
+            }
         }
 
         private void Start()
@@ -83,7 +139,102 @@ namespace NetStalkerAvalonia.Services.Implementations.BlockingRedirection
 
         private void SpoofClients()
         {
-            // TODO: Implement client spoofing logic
+            foreach (var client in _clients!)
+            {
+                if (client.Blocked
+                    && client.IsLocalDevice() == false
+                    && client.IsGateway() == false)
+                {
+                    ConstructAndSendArp(client, ArpPacketType.Spoof);
+                    if (ApplicationConfiguration.SpoofProtection)
+                        ConstructAndSendArp(client, ArpPacketType.Protection);
+                }
+            }
+        }
+
+        private void ConstructAndSendArp(Device device, ArpPacketType arpPacketType)
+        {
+            switch (arpPacketType)
+            {
+                case ArpPacketType.Spoof:
+                    var arpPacketForVicSpoof = new ArpPacket(ArpOperation.Request,
+                        targetHardwareAddress: device.Mac,
+                        targetProtocolAddress: device.IP,
+                        senderHardwareAddress: HostInfo.HostMac,
+                        senderProtocolAddress: HostInfo.GatewayIp);
+
+                    var etherPacketForVicSpoof = new EthernetPacket(
+                        sourceHardwareAddress: HostInfo.HostMac,
+                        destinationHardwareAddress: device.Mac,
+                        EthernetType.Arp)
+                    {
+                        PayloadPacket = arpPacketForVicSpoof
+                    };
+
+                    var arpPacketForGatewaySpoof = new ArpPacket(ArpOperation.Request,
+                        targetHardwareAddress: HostInfo.GatewayMac,
+                        targetProtocolAddress: HostInfo.GatewayIp,
+                        senderHardwareAddress: HostInfo.HostMac,
+                        senderProtocolAddress: device.IP);
+
+                    var etherPacketForGatewaySpoof = new EthernetPacket(
+                        sourceHardwareAddress: HostInfo.HostMac,
+                        destinationHardwareAddress: HostInfo.GatewayMac,
+                        EthernetType.Arp)
+                    {
+                        PayloadPacket = arpPacketForGatewaySpoof
+                    };
+
+                    if (device.Blocked)
+                        _device.SendPacket(etherPacketForVicSpoof);
+
+                    if (device.Redirected)
+                        _device.SendPacket(etherPacketForGatewaySpoof);
+
+                    break;
+                case ArpPacketType.Protection:
+                    var arpPacketForVicProtection = new ArpPacket(ArpOperation.Response,
+                        targetHardwareAddress: HostInfo.HostMac,
+                        targetProtocolAddress: HostInfo.HostIp,
+                        senderHardwareAddress: device.Mac,
+                        senderProtocolAddress: device.IP);
+
+                    var etherPacketForVicProtection = new EthernetPacket(
+                        sourceHardwareAddress: device.Mac,
+                        destinationHardwareAddress: HostInfo.HostMac,
+                        EthernetType.Arp)
+                    {
+                        PayloadPacket = arpPacketForVicProtection
+                    };
+
+                    var arpPacketForGatewayProtection = new ArpPacket(ArpOperation.Response,
+                        targetHardwareAddress: HostInfo.HostMac,
+                        targetProtocolAddress: HostInfo.HostIp,
+                        senderHardwareAddress: HostInfo.GatewayMac,
+                        senderProtocolAddress: HostInfo.GatewayIp);
+
+                    var etherPacketForGatewayProtection = new EthernetPacket(
+                        sourceHardwareAddress: HostInfo.GatewayMac,
+                        destinationHardwareAddress: HostInfo.HostMac,
+                        EthernetType.Arp)
+                    {
+                        PayloadPacket = arpPacketForGatewayProtection
+                    };
+
+                    _device.SendPacket(etherPacketForGatewayProtection);
+                    _device.SendPacket(etherPacketForVicProtection);
+                    break;
+            }
+        }
+
+        private void Pause()
+        {
+            if ((bool)_clients?.Any(client => client.Blocked || client.Redirected) == false)
+            {
+                // If no clients have active blocking or redirection we pause the service 
+                // so we don't do extra work on idle
+                Stop();
+            }
         }
 
         private void Stop()
@@ -93,18 +244,9 @@ namespace NetStalkerAvalonia.Services.Implementations.BlockingRedirection
             _device?.StopCapture();
         }
 
-        public void Destroy()
-        {
-            Stop();
+        #endregion
 
-            if (_device != null)
-            {
-                _device.Close();
-                _device.OnPacketArrival -= DeviceOnOnPacketArrival;
-                _device.Dispose();
-                _device = null;
-            }
-        }
+        #region API
 
         public void Block(Device device)
         {
@@ -121,13 +263,30 @@ namespace NetStalkerAvalonia.Services.Implementations.BlockingRedirection
         public void UnBlock(Device device)
         {
             device.UnBlock();
-            Start();
+            Pause();
         }
 
         public void UnRedirect(Device device)
         {
             device.UnRedirect();
-            Start();
+            Pause();
         }
+
+        public void Dispose()
+        {
+            Stop();
+
+            if (_device != null)
+            {
+                _device.Close();
+                _device.OnPacketArrival -= DeviceOnOnPacketArrival;
+                _device.Dispose();
+                _device = null;
+                _cancellationTokenSource?.Dispose();
+                _cancellationTokenSource = null;
+            }
+        }
+
+        #endregion
     }
 }
