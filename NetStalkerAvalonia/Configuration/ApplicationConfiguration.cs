cf09a2373e1c8f3852f61e13c3d1cac17c9b8fb0
HMZ <hamzi1995@gmail.com>
2022-05-29 21:58:20 +0300

    No need for this class as app config are pulled from config file

--- NetStalkerAvalonia/Configuration/ApplicationConfiguration.cs
+++
@@ -1,7 +0,0 @@
-namespace NetStalkerAvalonia.Configuration;
-
-public class ApplicationConfiguration
-{
-    public static bool SpoofProtection { get; set; }
-    public static bool SuppressNotifications { get; set; }
-}
