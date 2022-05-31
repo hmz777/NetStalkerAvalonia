b6d542cf5e3c3428386447f5f3990e948ffcef7f
HMZ <hamzi1995@gmail.com>
2022-05-31 22:34:50 +0300

    Update the adapter window

--- NetStalkerAvalonia/ViewModels/AdapterSelectViewModel.cs
+++ NetStalkerAvalonia/ViewModels/AdapterSelectViewModel.cs
@@ -1,16 +1,21 @@
 using System.Reactive;
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.ReactiveUI;
 using ReactiveUI;
 
 namespace NetStalkerAvalonia.ViewModels;
 
 public class AdapterSelectViewModel : ViewModelBase
 {
+    public ReactiveCommand<Window, Unit> Accept { get; set; }
     public ReactiveCommand<Unit, Unit> Exit { get; set; }
 
     public AdapterSelectViewModel()
     {
+        Accept = ReactiveCommand.Create((Window window) => { window.Close(); });
+
         Exit = ReactiveCommand.Create(() =>
         {
             var app = Application.Current?.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime;
