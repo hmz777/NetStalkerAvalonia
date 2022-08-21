9b53b0d5bcac53c25585073f83cfafe260683577
HMZ <hamzi1995@gmail.com>
2022-08-21 15:06:20 +0300

    Add context menu button for devices + behavior for handling context menu on left click and disabling context menu on right click

---
+++ NetStalkerAvalonia/Behaviors/ContextMenuDisableRightClickBehavior.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Windows;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data;
+using Avalonia.Interactivity;
+using RoutedEventArgs = Avalonia.Interactivity.RoutedEventArgs;
+
+namespace NetStalkerAvalonia.Behaviors;
+
+public class ContextMenuDisableRightClickBehavior : AvaloniaObject
+{
+    static ContextMenuDisableRightClickBehavior()
+    {
+        IsContextMenuRightClickDisabledProperty.Changed.Subscribe(
+            (x) => { OnIsContextMenuRightClickDisabledChanged(x.Sender, x.NewValue.GetValueOrDefault<bool>()); });
+    }
+
+    public static readonly StyledProperty<bool> IsContextMenuRightClickDisabledProperty =
+        AvaloniaProperty
+            .RegisterAttached<ContextMenuDisableRightClickBehavior, Interactive, bool>(
+                "IsContextMenuRightClickDisabled", default(bool), false, BindingMode.OneTime);
+
+    public static void SetIsContextMenuRightClickDisabled(AvaloniaObject element, bool value)
+    {
+        element.SetValue(IsContextMenuRightClickDisabledProperty, value);
+    }
+
+    public static bool GetIsContextMenuRightClickDisabled(AvaloniaObject element)
+    {
+        return (bool)element.GetValue(IsContextMenuRightClickDisabledProperty);
+    }
+
+    private static void OnIsContextMenuRightClickDisabledChanged(IAvaloniaObject sender, bool e)
+    {
+        var element = sender as Control;
+
+        if (element != null)
+        {
+            var isEnabled = e;
+
+            if (isEnabled)
+            {
+                element.ContextRequested += OnMouseRightButtonUp;
+            }
+            else
+            {
+                element.ContextRequested -= OnMouseRightButtonUp;
+            }
+        }
+    }
+
+    private static void OnMouseRightButtonUp(object sender, ContextRequestedEventArgs e)
+    {
+        e.Handled = true;
+    }
+}
