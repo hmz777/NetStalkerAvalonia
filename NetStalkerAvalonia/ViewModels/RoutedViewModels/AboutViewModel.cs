52f9473445556fe4849c44ed83da709027cc42f0
HMZ <hamzi1995@gmail.com>
2022-12-07 18:12:33 +0300

    Update the about section

--- NetStalkerAvalonia/ViewModels/RoutedViewModels/AboutViewModel.cs
+++ NetStalkerAvalonia/ViewModels/RoutedViewModels/AboutViewModel.cs
@@ -1,17 +1,30 @@
 ﻿using ReactiveUI;
+using System.Reflection;
 
 namespace NetStalkerAvalonia.ViewModels.RoutedViewModels
 {
-    public class AboutViewModel : ViewModelBase, IRoutableViewModel
-    {
-        public string? UrlPathSegment { get; } = "About";
-        public IScreen? HostScreen { get; }
+	public class AboutViewModel : ViewModelBase, IRoutableViewModel
+	{
+		public string? UrlPathSegment { get; } = "About";
+		public IScreen? HostScreen { get; }
 
-        #region Constructors
+		#region Constructors
 
-        public AboutViewModel(){}
-        public AboutViewModel(IScreen screen) => HostScreen = screen;
+		public AboutViewModel() { }
+		public AboutViewModel(IScreen screen)
+		{
+			HostScreen = screen;
 
-        #endregion
-    }
+			AppVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+		}
+
+		#endregion
+
+		private string _appVersion;
+		public string AppVersion
+		{
+			get => _appVersion;
+			set => this.RaiseAndSetIfChanged(ref _appVersion, value);
+		}
+	}
 }
--- NetStalkerAvalonia/Views/RoutedViews/AboutView.axaml.cs
+++ NetStalkerAvalonia/Views/RoutedViews/AboutView.axaml.cs
@@ -1,21 +1,80 @@
+using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
 using NetStalkerAvalonia.ViewModels.RoutedViewModels;
 using ReactiveUI;
+using System.Diagnostics;
 
 namespace NetStalkerAvalonia.Views.RoutedViews
 {
-    public partial class AboutView : ReactiveUserControl<AboutViewModel>
-    {
-        public AboutView()
-        {
-            this.WhenActivated(disposables => { });
-            InitializeComponent();
-        }
+	public partial class AboutView : ReactiveUserControl<AboutViewModel>
+	{
+		public AboutView()
+		{
+			this.WhenActivated(disposables =>
+			{
+				var linksContainer = this.Find<StackPanel>("links");
 
-        private void InitializeComponent()
-        {
-            AvaloniaXamlLoader.Load(this);
-        }
-    }
+				foreach (var control in linksContainer.Children)
+				{
+					if (control.GetType() == typeof(StackPanel))
+					{
+						var stackPanel = control as StackPanel;
+
+						foreach (var innerControl in stackPanel.Children)
+						{
+							if (innerControl.GetType() == typeof(TextBlock))
+							{
+								var textBlock = innerControl as TextBlock;
+								textBlock.PointerPressed += TextBlock_PointerPressed;
+							}
+						}
+					}
+				}
+
+				var social = this.Find<StackPanel>("sociallinks");
+
+				foreach (var control in social.Children)
+				{
+					control.PointerPressed += Control_PointerPressed;
+				}
+
+			});
+
+			InitializeComponent();
+		}
+
+		private void Control_PointerPressed(object? sender, Avalonia.Input.PointerPressedEventArgs e)
+		{
+			var control = sender as Control;
+
+			if (e.GetCurrentPoint(control).Properties.IsLeftButtonPressed)
+			{
+				Process.Start(new ProcessStartInfo
+				{
+					FileName = control!.Tag!.ToString(),
+					UseShellExecute = true
+				});
+			}
+		}
+
+		private void TextBlock_PointerPressed(object? sender, Avalonia.Input.PointerPressedEventArgs e)
+		{
+			var textBlock = sender as TextBlock;
+
+			if (e.GetCurrentPoint(textBlock).Properties.IsLeftButtonPressed)
+			{
+				System.Diagnostics.Process.Start(new ProcessStartInfo
+				{
+					FileName = textBlock.Text,
+					UseShellExecute = true
+				});
+			}
+		}
+
+		private void InitializeComponent()
+		{
+			AvaloniaXamlLoader.Load(this);
+		}
+	}
 }
