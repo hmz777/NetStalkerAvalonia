2a7fcb2298af1fa7db588b81ede85e46eb3bbb53
HMZ <hamzi1995@gmail.com>
2022-12-26 17:10:24 +0300

    Add LinkButton component for opening links

--- NetStalkerAvalonia/Views/RoutedViews/AboutView.axaml.cs
+++ NetStalkerAvalonia/Views/RoutedViews/AboutView.axaml.cs
@@ -1,9 +1,12 @@
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
+using Avalonia.VisualTree;
+using NetStalkerAvalonia.Helpers;
 using NetStalkerAvalonia.ViewModels.RoutedViewModels;
 using ReactiveUI;
 using System.Diagnostics;
+using System.Linq;
 
 namespace NetStalkerAvalonia.Views.RoutedViews
 {
@@ -13,63 +16,9 @@ namespace NetStalkerAvalonia.Views.RoutedViews
 		{
 			this.WhenActivated(disposables =>
 			{
-				var linksContainer = this.Find<StackPanel>("links");
-
-				foreach (var control in linksContainer.Children)
-				{
-					if (control.GetType() == typeof(StackPanel))
-					{
-						var stackPanel = control as StackPanel;
-
-						foreach (var innerControl in stackPanel.Children)
-						{
-							if (innerControl.GetType() == typeof(TextBlock))
-							{
-								var textBlock = innerControl as TextBlock;
-								textBlock.PointerPressed += TextBlock_PointerPressed;
-							}
-						}
-					}
-				}
-
-				var social = this.Find<StackPanel>("sociallinks");
-
-				foreach (var control in social.Children)
-				{
-					control.PointerPressed += Control_PointerPressed;
-				}
-
 			});
 
 			InitializeComponent();
-		}
-
-		private void Control_PointerPressed(object? sender, Avalonia.Input.PointerPressedEventArgs e)
-		{
-			var control = sender as Control;
-
-			if (e.GetCurrentPoint(control).Properties.IsLeftButtonPressed)
-			{
-				Process.Start(new ProcessStartInfo
-				{
-					FileName = control!.Tag!.ToString(),
-					UseShellExecute = true
-				});
-			}
-		}
-
-		private void TextBlock_PointerPressed(object? sender, Avalonia.Input.PointerPressedEventArgs e)
-		{
-			var textBlock = sender as TextBlock;
-
-			if (e.GetCurrentPoint(textBlock).Properties.IsLeftButtonPressed)
-			{
-				System.Diagnostics.Process.Start(new ProcessStartInfo
-				{
-					FileName = textBlock.Text,
-					UseShellExecute = true
-				});
-			}
 		}
 
 		private void InitializeComponent()
