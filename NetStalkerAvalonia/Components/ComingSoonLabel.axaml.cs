d85c0536fc5ca58b75e5c4fc21e86b56baa7c8af
HMZ <hamzi1995@gmail.com>
2023-01-12 07:31:46 +0300

    Add a coming soon label so we don't just leave empty views without indication

---
+++ NetStalkerAvalonia/Components/ComingSoonLabel.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace NetStalkerAvalonia.Components
+{
+	public partial class ComingSoonLabel : UserControl
+	{
+		public ComingSoonLabel()
+		{
+			InitializeComponent();
+		}
+	}
+}
