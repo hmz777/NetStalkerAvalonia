7ec6b8dcbf66c1fc19e28f90fd13048af0b67e07
HMZ <hamzi1995@gmail.com>
2023-01-15 02:15:26 +0300

    Use the app theme as a singleton

--- NetStalkerAvalonia/Theme/AppTheme.cs
+++ NetStalkerAvalonia/Theme/AppTheme.cs
@@ -1,28 +1,41 @@
 ﻿namespace NetStalkerAvalonia.Theme
 {
-    public class AppTheme
-    {
-        public static string WindowWidth => "1000";
-        public static string WindowMinWidth => "850";
-        public static string WindowHeight => "600";
-        public static string WindowMinHeight => "450";
-        public static string NavBorderColor => "#2C2C2C";
-        public static string NavBackground => "#1E1E1E";
-        public static string NavElementBackground => "#2C2C2C";
-        public static string NavElementBorder => "#1F1F1F";
-        public static string NavElementText => "#ffffff";
-        public static string WindowBackground => "#1E1E1E";
-        public static string TitleBarBackColor => "#000000";
-        public static string TitleBarButtonBackColor => "#000000";
-        public static string TitleBarButtonFrontColor => "#FFFFFF";
-        public static string TitleBarButtonsHoverColor => "#1F1F1F";
-        public static string PageTitleColor => "white";
-        public static string AccentColor => "Teal";
-        public static string PageTitleSize => "25";
-        public static string ListRowSelectedBackColor => "Teal";
-        public static string ListColumnHeaderBackColor => "#2C2C2C";
-        public static string PagePadding => "15";
-        public static string DialogButtonWidth => "60";
-        public static string DialogButtonHeight => "35";
-    }
+	public class AppTheme
+	{
+		private static AppTheme? _instance;
+
+		public AppTheme() { }
+
+		public static AppTheme Instance
+		{
+			get
+			{
+				_instance ??= new AppTheme();
+				return _instance;
+			}
+		}
+
+		public string WindowWidth => "1000";
+		public string WindowMinWidth => "850";
+		public string WindowHeight => "600";
+		public string WindowMinHeight => "450";
+		public string NavBorderColor => "#2C2C2C";
+		public string NavBackground => "#1E1E1E";
+		public string NavElementBackground => "#2C2C2C";
+		public string NavElementBorder => "#1F1F1F";
+		public string NavElementText => "#ffffff";
+		public string WindowBackground => "#1E1E1E";
+		public string TitleBarBackColor => "#000000";
+		public string TitleBarButtonBackColor => "#000000";
+		public string TitleBarButtonFrontColor => "#FFFFFF";
+		public string TitleBarButtonsHoverColor => "#1F1F1F";
+		public string PageTitleColor => "white";
+		public string AccentColor => "Teal";
+		public string PageTitleSize => "25";
+		public string ListRowSelectedBackColor => "Teal";
+		public string ListColumnHeaderBackColor => "#2C2C2C";
+		public string PagePadding => "15";
+		public string DialogButtonWidth => "60";
+		public string DialogButtonHeight => "35";
+	}
 }
--- NetStalkerAvalonia/ViewModels/ViewModelBase.cs
+++ NetStalkerAvalonia/ViewModels/ViewModelBase.cs
@@ -3,16 +3,8 @@ using ReactiveUI;
 
 namespace NetStalkerAvalonia.ViewModels
 {
-    public class ViewModelBase : ReactiveObject
-    {
-        public AppTheme Theme { get; set; } = new();
-
-        // private bool _navEnabled;
-        //
-        // public bool NavEnabled
-        // {
-        //     get => _navEnabled;
-        //     set => this.RaiseAndSetIfChanged(ref _navEnabled, value);
-        // }
-    }
+	public class ViewModelBase : ReactiveObject
+	{
+		public AppTheme Theme => AppTheme.Instance;
+	}
 }
