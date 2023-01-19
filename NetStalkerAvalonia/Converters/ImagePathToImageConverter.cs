11cbc2ed7a012f704a3b8bc32b5587054f85a1a5
HMZ <hamzi1995@gmail.com>
2023-01-19 17:35:22 +0300

    Add ImagePathToImageConverter to return an image from a supplied path

---
+++ NetStalkerAvalonia/Converters/ImagePathToImageConverter.cs
@@ -0,0 +1,40 @@
+﻿using Avalonia;
+using Avalonia.Data.Converters;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using Splat;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetStalkerAvalonia.Converters
+{
+	public class ImagePathToImageConverter : IValueConverter
+	{
+		public readonly static ImagePathToImageConverter Instance = new();
+
+		public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+		{
+			if (value is null)
+			{
+				return null;
+			}
+
+			var assets = AvaloniaLocator.Current.GetService<IAssetLoader>();
+			return new Bitmap(assets.Open(
+			new Uri(
+				string.Format("avares://{0}{1}",
+					Assembly.GetExecutingAssembly().GetName().Name,
+					value))));
+		}
+
+		public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
