aa88177b41f86e222f7c4a4530dbfa1db46a8ed1
HMZ <hamzi1995@gmail.com>
2023-01-20 02:46:03 +0300

    Revert "Refactor"
    This reverts commit 164d3869b4945f859508d63a3418d22d93630f66.

--- NetStalkerAvalonia/Views/RoutedViews/RuleBuilderView.axaml.cs
+++ NetStalkerAvalonia/Views/RoutedViews/RuleBuilderView.axaml.cs
@@ -1,56 +1,21 @@
-using Avalonia.Controls.Mixins;
 using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
-using NetStalkerAvalonia.ViewModels;
-using NetStalkerAvalonia.ViewModels.InteractionViewModels;
 using NetStalkerAvalonia.ViewModels.RoutedViewModels;
 using ReactiveUI;
-using System.Reactive;
-using System.Threading.Tasks;
 
 namespace NetStalkerAvalonia.Views.RoutedViews
 {
-	public partial class RuleBuilderView : ReactiveUserControl<RuleBuilderViewModel>
-	{
-		public RuleBuilderView()
-		{
-			this.WhenActivated(d =>
-			{
-				ViewModel!.ShowAddRuleDialog.RegisterHandler(DoShowAddDialogAsync).DisposeWith(d);
-				ViewModel!.ShowUpdateRuleDialog.RegisterHandler(DoShowUpdateDialogAsync).DisposeWith(d);
-			});
+    public partial class RuleBuilderView : ReactiveUserControl<RuleBuilderViewModel>
+    {
+        public RuleBuilderView()
+        {
+            this.WhenActivated(disposables => { });
+            InitializeComponent();
+        }
 
-			InitializeComponent();
-		}
-
-		private void InitializeComponent()
-		{
-			AvaloniaXamlLoader.Load(this);
-		}
-
-		private async Task DoShowAddDialogAsync(InteractionContext<Unit, AddUpdateRuleModel?> interaction)
-		{
-			var dialog = new AddUpdateRuleWindow
-			{
-				DataContext = new AddUpdateRuleViewModel()
-			};
-
-			var result = await dialog.ShowDialog<AddUpdateRuleModel?>(StaticData.MainWindow);
-			interaction.SetOutput(result);
-		}
-
-		private async Task DoShowUpdateDialogAsync(InteractionContext<AddUpdateRuleModel, AddUpdateRuleModel> interaction)
-		{
-			var dialog = new AddUpdateRuleWindow
-			{
-				DataContext = new AddUpdateRuleViewModel()
-				{
-					AddUpdateRuleModel = interaction.Input
-				}
-			};
-
-			var result = await dialog.ShowDialog<AddUpdateRuleModel>(StaticData.MainWindow);
-			interaction.SetOutput(result);
-		}
-	}
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
 }
