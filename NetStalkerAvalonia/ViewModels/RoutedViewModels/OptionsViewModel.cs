de5ee5aaa471977d1a1472f1d94c027da38d2377
HMZ <hamzi1995@gmail.com>
2023-01-25 13:52:34 +0300

    Reflect the use of the static show message method in the options section when displaying status messages instead of directly using the message bus

--- NetStalkerAvalonia/ViewModels/RoutedViewModels/OptionsViewModel.cs
+++ NetStalkerAvalonia/ViewModels/RoutedViewModels/OptionsViewModel.cs
@@ -1,16 +1,13 @@
-﻿using NetStalkerAvalonia.Configuration;
-using NetStalkerAvalonia.Helpers;
+﻿using NetStalkerAvalonia.Helpers;
 using NetStalkerAvalonia.Models;
 using NetStalkerAvalonia.Services;
 using NetStalkerAvalonia.ViewModels.InteractionViewModels;
 using ReactiveUI;
-using System;
 using System.Reactive;
-using System.Reactive.Linq;
 
 namespace NetStalkerAvalonia.ViewModels.RoutedViewModels
 {
-    public class OptionsViewModel : ViewModelBase, IRoutableViewModel
+	public class OptionsViewModel : ViewModelBase, IRoutableViewModel
 	{
 		#region Routing
 
@@ -79,17 +76,17 @@ namespace NetStalkerAvalonia.ViewModels.RoutedViewModels
 		public void SetPasswordImpl()
 		{
 			if (IsAppLocked && string.IsNullOrWhiteSpace(CurrentPassword))
-				MessageBus.Current.SendMessage<StatusMessageModel>(new StatusMessageModel(MessageType.Error, "Current password is incorrect!"), ContractKeys.StatusMessage.ToString());
+				Tools.ShowMessage(new StatusMessageModel(MessageType.Error, "Current password is incorrect!"));
 			else if (string.IsNullOrWhiteSpace(NewPassword))
-				MessageBus.Current.SendMessage<StatusMessageModel>(new StatusMessageModel(MessageType.Error, "New password is invalid!"), ContractKeys.StatusMessage.ToString());
+				Tools.ShowMessage(new StatusMessageModel(MessageType.Error, "New password is invalid!"));
 			else
 			{
 				var result = _appLockService.SetPassword(NewPassword, CurrentPassword);
 
 				if (result == false)
-					MessageBus.Current.SendMessage<StatusMessageModel>(new StatusMessageModel(MessageType.Error, "Unable to set the password!"), ContractKeys.StatusMessage.ToString());
+					Tools.ShowMessage(new StatusMessageModel(MessageType.Error, "Unable to set the password!"));
 				else
-					MessageBus.Current.SendMessage<StatusMessageModel>(new StatusMessageModel(MessageType.Success, "Password has been set!"), ContractKeys.StatusMessage.ToString());
+					Tools.ShowMessage(new StatusMessageModel(MessageType.Success, "Password has been set!"));
 			}
 
 			ClearPasswordFields();
@@ -98,15 +95,15 @@ namespace NetStalkerAvalonia.ViewModels.RoutedViewModels
 		public void ClearPasswordImpl()
 		{
 			if (string.IsNullOrWhiteSpace(CurrentPassword))
-				MessageBus.Current.SendMessage<StatusMessageModel>(new StatusMessageModel(MessageType.Error, "Current password is incorrect!"), ContractKeys.StatusMessage.ToString());
+				Tools.ShowMessage(new StatusMessageModel(MessageType.Error, "Current password is incorrect!"));
 			else
 			{
 				var result = _appLockService.ClearPassword(CurrentPassword);
 
 				if (result == false)
-					MessageBus.Current.SendMessage<StatusMessageModel>(new StatusMessageModel(MessageType.Error, "Unable to clear the password!"), ContractKeys.StatusMessage.ToString());
+					Tools.ShowMessage(new StatusMessageModel(MessageType.Error, "Unable to clear the password!"));
 				else
-					MessageBus.Current.SendMessage<StatusMessageModel>(new StatusMessageModel(MessageType.Success, "Password has been cleared!"), ContractKeys.StatusMessage.ToString());
+					Tools.ShowMessage(new StatusMessageModel(MessageType.Success, "Password has been cleared!"));
 			}
 
 			ClearPasswordFields();
