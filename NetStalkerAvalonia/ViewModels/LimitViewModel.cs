6e2eb7b46c54daee20e92b4949f1dbdb2dd5a130
HMZ <hamzi1995@gmail.com>
2023-02-12 16:21:04 +0300

    Refactor and add restriction on the limitation so we don't get negative values

--- NetStalkerAvalonia/ViewModels/LimitViewModel.cs
+++ NetStalkerAvalonia/ViewModels/LimitViewModel.cs
@@ -1,4 +1,5 @@
 using System.Reactive;
+using System.Reactive.Linq;
 using NetStalkerAvalonia.ViewModels.InteractionViewModels;
 using ReactiveUI;
 
@@ -6,12 +7,16 @@ namespace NetStalkerAvalonia.ViewModels;
 
 public class LimitViewModel : ViewModelBase
 {
-    public ReactiveCommand<Unit, DeviceLimitsModel?> Apply { get; set; }
-    public DeviceLimitsModel? DeviceLimits { get; set; }
+	public ReactiveCommand<Unit, DeviceLimitsModel> Apply { get; set; }
+	public DeviceLimitsModel DeviceLimits { get; set; }
 
-    public LimitViewModel()
-    {
-        Apply = ReactiveCommand.Create(() => { return DeviceLimits; });
-        DeviceLimits = new DeviceLimitsModel(0, 0);
-    }
+	public LimitViewModel()
+	{
+		DeviceLimits = new DeviceLimitsModel(0, 0);
+
+		var capApplyExecute = this.WhenAnyValue(x => x.DeviceLimits)
+			.Select(x => x.Upload >= 0 && x.Download >= 0);
+
+		Apply = ReactiveCommand.Create(() => { return DeviceLimits; }, capApplyExecute);
+	}
 }
