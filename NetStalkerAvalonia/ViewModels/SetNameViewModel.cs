77bd691dbabe9859436336ee8f4940e93f52c538
HMZ <hamzi1995@gmail.com>
2023-02-12 16:21:33 +0300

    Remove usings and white spaces

--- NetStalkerAvalonia/ViewModels/SetNameViewModel.cs
+++ NetStalkerAvalonia/ViewModels/SetNameViewModel.cs
@@ -1,18 +1,16 @@
-using System.Reactive;
-using System.Windows.Documents.DocumentStructures;
-using Windows.System.Profile;
 using ReactiveUI;
+using System.Reactive;
 
 namespace NetStalkerAvalonia.ViewModels;
 
 public class SetNameViewModel : ViewModelBase
 {
-    public string? Name { get; set; }
+	public string? Name { get; set; }
 
-    public ReactiveCommand<Unit, string> Accept { get; set; }
+	public ReactiveCommand<Unit, string> Accept { get; set; }
 
-    public SetNameViewModel()
-    {
-        Accept = ReactiveCommand.Create(() => Name!);
-    }
+	public SetNameViewModel()
+	{
+		Accept = ReactiveCommand.Create(() => Name!);
+	}
 }
