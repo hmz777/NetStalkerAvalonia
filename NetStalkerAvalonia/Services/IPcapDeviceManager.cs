945a1695ee62be4e3f816e4815ed5a1645c0db62
HMZ <hamzi1995@gmail.com>
2023-02-14 17:41:52 +0300

    Create a factory for Pcap devices

---
+++ NetStalkerAvalonia/Services/IPcapDeviceManager.cs
@@ -0,0 +1,18 @@
+﻿using SharpPcap;
+using SharpPcap.LibPcap;
+using System;
+
+namespace NetStalkerAvalonia.Services
+{
+	public interface IPcapDeviceManager
+	{
+		/// <summary>
+		/// The created device is automatically opened
+		/// </summary>
+		/// <param name="filter"></param>
+		/// <param name="packetArrivalHandler"></param>
+		/// <returns></returns>
+		public LibPcapLiveDevice CreateDevice(string filter, PacketArrivalEventHandler packetArrivalHandler);
+
+	}
+}
---
+++ NetStalkerAvalonia/Services/Implementations/PcapDeviceManagement/PcapDeviceManager.cs
@@ -0,0 +1,33 @@
+﻿using NetStalkerAvalonia.Models;
+using SharpPcap;
+using SharpPcap.LibPcap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetStalkerAvalonia.Services.Implementations.PcapDeviceManagement
+{
+	public class PcapDeviceManager : IPcapDeviceManager
+	{
+		private readonly string _adapterName;
+
+		public PcapDeviceManager()
+		{
+			_adapterName = (from devicex in LibPcapLiveDeviceList.Instance
+							where devicex.Interface.FriendlyName == HostInfo.NetworkAdapterName
+							select devicex).ToList()[0].Name;
+		}
+
+		public LibPcapLiveDevice CreateDevice(string filter, PacketArrivalEventHandler packetArrivalHandler)
+		{
+			var device = LibPcapLiveDeviceList.New()[_adapterName];
+			device.Open(DeviceModes.Promiscuous, 1000);
+			device.Filter = filter;
+			device.OnPacketArrival += packetArrivalHandler;
+
+			return device;
+		}
+	}
+}
