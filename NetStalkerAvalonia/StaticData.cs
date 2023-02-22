282bfc421da205f7bfa24e65751282b61d8c0bca
HMZ <hamzi1995@gmail.com>
2023-02-22 11:16:52 +0300

    Remove the static view model initialization since they're registered with the DI container

--- NetStalkerAvalonia/StaticData.cs
+++ NetStalkerAvalonia/StaticData.cs
@@ -8,23 +8,5 @@ namespace NetStalkerAvalonia
 	public class StaticData
 	{
 		public static Window? MainWindow { get; set; }
-
-		public static List<ViewModelBase> ViewModels = new();
-
-		public static void InitRoutedViewModels()
-		{
-			var mainViewModel = new MainWindowViewModel();
-
-			ViewModels = new List<ViewModelBase>
-		{
-			mainViewModel,
-			new SnifferViewModel(mainViewModel),
-			new OptionsViewModel(mainViewModel),
-			new RuleBuilderViewModel(mainViewModel),
-			new HelpViewModel(mainViewModel),
-			new AboutViewModel(mainViewModel),
-			new AppLogViewModel()
-		};
-		}
 	}
 }
