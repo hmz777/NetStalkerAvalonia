aa2ab2a3eccf342b35817ce7f75a8441d51a63a8
HMZ <hamzi1995@gmail.com>
2023-02-27 14:57:08 +0300

    Optimize the BlockerRedirector tests

--- NetStalker.Tests/ServiceSpecs/BlockerRedirectorSpec.cs
+++ NetStalker.Tests/ServiceSpecs/BlockerRedirectorSpec.cs
@@ -1,51 +1,63 @@
 ﻿using FluentAssertions;
 using NetStalker.Tests.AutoData;
 using NetStalkerAvalonia.Models;
+using NetStalkerAvalonia.Services;
+using NetStalkerAvalonia.Services.Implementations.BlockingRedirection;
 
 namespace NetStalker.Tests.ServiceSpecs
 {
-	public class BlockerRedirectorSpec : BlockerRedirectorSpecBase
+	public class BlockerRedirectorSpec
 	{
-		[Theory, AutoDeviceData]
-		public void Can_Block_A_Device(Device device)
+		[Theory, AutoServiceData]
+		public void Can_Block_A_Device(IRuleService ruleService, IPcapDeviceManager pcapDeviceManager, Device device)
 		{
-			BlockerRedirectorService.Block(device);
+			var sut = new BlockerRedirector(ruleService, pcapDeviceManager);
+
+			sut.Block(device);
 
 			device.Blocked.Should().Be(true);
 			device.Redirected.Should().Be(false);
 		}
 
-		[Theory, AutoDeviceData]
-		public void Can_UnBlock_A_Device(Device device)
+		[Theory, AutoServiceData]
+		public void Can_UnBlock_A_Device(IRuleService ruleService, IPcapDeviceManager pcapDeviceManager, Device device)
 		{
-			BlockerRedirectorService.UnBlock(device);
+			var sut = new BlockerRedirector(ruleService, pcapDeviceManager);
+
+			sut.UnBlock(device);
 
 			device.Blocked.Should().Be(false);
 			device.Redirected.Should().Be(false);
 		}
 
-		[Theory, AutoDeviceData]
-		public void Can_Redirect_A_Device(Device device)
+		[Theory, AutoServiceData]
+		public void Can_Redirect_A_Device(IRuleService ruleService, IPcapDeviceManager pcapDeviceManager, Device device)
 		{
-			BlockerRedirectorService.Redirect(device);
+			var sut = new BlockerRedirector(ruleService, pcapDeviceManager);
+
+			sut.Redirect(device);
 
 			device.Redirected.Should().Be(true);
 			device.Blocked.Should().Be(false);
 		}
 
-		[Theory, AutoDeviceData]
-		public void Can_UnRedirect_A_Device(Device device)
+		[Theory, AutoServiceData]
+		public void Can_UnRedirect_A_Device(IRuleService ruleService, IPcapDeviceManager pcapDeviceManager, Device device)
 		{
-			BlockerRedirectorService.UnRedirect(device);
+			var sut = new BlockerRedirector(ruleService, pcapDeviceManager);
+
+			sut.UnRedirect(device);
 
 			device.Redirected.Should().Be(false);
 			device.Blocked.Should().Be(false);
 		}
 
-		[Theory, AutoDeviceData]
-		public void Can_Limit_A_Device(Device device)
+		[Theory, AutoServiceData]
+		public void Can_Limit_A_Device(IRuleService ruleService, IPcapDeviceManager pcapDeviceManager, Device device)
 		{
-			BlockerRedirectorService.Limit(device, 15, 12);
+			var sut = new BlockerRedirector(ruleService, pcapDeviceManager);
+
+			sut.Limit(device, 15, 12);
 
 			device.Redirected.Should().Be(true);
 			device.DownloadCap.Should().Be(15 * 1024);
