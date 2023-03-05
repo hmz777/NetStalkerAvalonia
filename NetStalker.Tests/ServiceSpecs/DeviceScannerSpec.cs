9cb8928c7cd2d89cb169fefed4f83c861deb34ac
HMZ <hamzi1995@gmail.com>
2023-03-05 17:19:40 +0300

    Add Device scanner tests

---
+++ NetStalker.Tests/ServiceSpecs/DeviceScannerSpec.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using NetStalker.Tests.AutoData;
+using NetStalkerAvalonia.Services;
+using NetStalkerAvalonia.Services.Implementations.DeviceScanning;
+
+namespace NetStalker.Tests.ServiceSpecs
+{
+	public class DeviceScannerSpec
+	{
+		[Theory, AutoServiceData]
+		public void Can_Start_Scanner(IPcapDeviceManager pcapDeviceManager, IDeviceNameResolver deviceNameResolver, IDeviceTypeIdentifier deviceTypeIdentifier)
+		{
+			var sut = new DeviceScanner(pcapDeviceManager, deviceNameResolver, deviceTypeIdentifier);
+
+			var action = () => sut.Scan();
+
+			action.Should().NotThrow<Exception>();
+			sut.Status.Should().Be(true);
+		}
+
+		[Theory, AutoServiceData]
+		public void Can_Do_A_Scan_Refresh(IPcapDeviceManager pcapDeviceManager, IDeviceNameResolver deviceNameResolver, IDeviceTypeIdentifier deviceTypeIdentifier)
+		{
+			var sut = new DeviceScanner(pcapDeviceManager, deviceNameResolver, deviceTypeIdentifier);
+			sut.Scan();
+
+			var action = () => sut.Refresh();
+
+			action.Should().NotThrow<Exception>();
+			sut.Status.Should().Be(true);
+		}
+	}
+}
