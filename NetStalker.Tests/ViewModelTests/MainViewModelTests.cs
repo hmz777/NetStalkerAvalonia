ccc29244ac528f7d8ac312700511ebd14e5ccc0a
HMZ <hamzi1995@gmail.com>
2023-03-10 19:03:37 +0300

    Add main viewmodel tests

---
+++ NetStalker.Tests/ViewModelTests/MainViewModelTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using NetStalker.Tests.AutoData;
+using NetStalkerAvalonia.Services;
+using NetStalkerAvalonia.ViewModels;
+using NetStalkerAvalonia.ViewModels.RoutedViewModels;
+
+namespace NetStalker.Tests.ViewModelTests
+{
+	public class MainViewModelTests
+	{
+		#region Routing
+
+		[Theory, AutoServiceData]
+		public void CanNavigateToRules(IRouter router, RuleBuilderViewModel ruleBuilderViewModel)
+		{
+			var sut = new MainViewModel(router, null, null, null, null, null, ruleBuilderViewModel, null, null, null);
+
+			sut.GoToRules.Execute();
+
+			sut.Router.CurrentViewModel.Subscribe(x => x.Should().Be(ruleBuilderViewModel));
+
+			sut.Dispose();
+		}
+
+		#endregion
+	}
+}
