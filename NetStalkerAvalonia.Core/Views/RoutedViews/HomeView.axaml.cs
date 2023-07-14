e7adf1a8b6cedcc4ec015cbdd30b73978afbf6e3
HMZ <hamzi1995@gmail.com>
2023-07-15 00:30:13 +0300

    Create the home view and vm to be separate from the main view and vm

--- NetStalkerAvalonia.Core/Views/RoutedViews/HomeView.axaml.cs
+++ NetStalkerAvalonia.Core/Views/RoutedViews/HomeView.axaml.cs
@@ -1,21 +1,56 @@
-using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
+using NetStalkerAvalonia.Core.Helpers;
 using NetStalkerAvalonia.Core.ViewModels;
+using NetStalkerAvalonia.Core.ViewModels.InteractionViewModels;
+using NetStalkerAvalonia.Core.ViewModels.RoutedViewModels;
 using ReactiveUI;
+using System.Reactive.Disposables;
+using System.Threading.Tasks;
 
 namespace NetStalkerAvalonia.Core.Views.RoutedViews
 {
-    public partial class HomeView : ReactiveUserControl<MainViewModel>
-    {
-        public HomeView()
-        {
-            this.WhenActivated(disposables => { });
-            InitializeComponent();
-        }
+	public partial class HomeView : ReactiveUserControl<HomeViewModel>
+	{
+		public HomeView()
+		{
+			this.WhenActivated(disposables =>
+			{
+				ViewModel!
+					.SetFriendlyNameInteraction!
+					.RegisterHandler(DoShowSetFriendlyDeviceNameDialogAsync)
+					.DisposeWith(disposables);
 
-        private void InitializeComponent()
-        {
-            AvaloniaXamlLoader.Load(this);
-        }
-    }
+				ViewModel!
+					.ShowLimitDialogInteraction!
+					.RegisterHandler(DoShowLimitDialogAsync)
+					.DisposeWith(disposables);
+
+			});
+
+			InitializeComponent();
+		}
+
+		private async Task DoShowSetFriendlyDeviceNameDialogAsync(InteractionContext<string?, string?> interaction)
+		{
+			var setNameDialogWindow = new SetNameView
+			{
+				DataContext = new SetNameViewModel() { Name = interaction.Input }
+			};
+
+			var result = await setNameDialogWindow.ShowDialog<string?>(StaticData.MainWindow);
+			interaction.SetOutput(result);
+		}
+
+		private async Task DoShowLimitDialogAsync(
+			InteractionContext<DeviceLimitsModel?, DeviceLimitsModel?> interaction)
+		{
+			var dialog = new LimitView
+			{
+				DataContext = new LimitViewModel() { DeviceLimits = interaction.Input }
+			};
+
+			var result = await dialog.ShowDialog<DeviceLimitsModel>(StaticData.MainWindow);
+			interaction.SetOutput(result);
+		}
+	}
 }
