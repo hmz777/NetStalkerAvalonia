c6d7dd2aa2bccf11cbaaf5bdcd3666f19c5ba7f3
HMZ <hamzi1995@gmail.com>
2023-07-17 16:03:15 +0300

    Add string equal and not equal converter so that we can disable nav buttons based on current page

---
+++ NetStalkerAvalonia.Core/Converters/StringEqualsConverter.cs
@@ -0,0 +1,43 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+using System.Linq;
+
+namespace NetStalkerAvalonia.Core.Converters
+{
+	public class StringEqualsConverter : IValueConverter
+	{
+		private string[] operators = new string[] { "!=", "=" };
+		public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+		{
+			if (value == null || parameter == null) return false;
+
+			var valueStr = value as string;
+
+			try
+			{
+				if (parameter.ToString()!.Split('|') is [var op, var str] &&
+					operators.Contains(op))
+				{
+					switch (op)
+					{
+						case "!=":
+							return valueStr != str;
+						case "=":
+							return valueStr == str;
+						default:
+							break;
+					};
+				}
+			}
+			catch { }
+
+			return false;
+		}
+
+		public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
